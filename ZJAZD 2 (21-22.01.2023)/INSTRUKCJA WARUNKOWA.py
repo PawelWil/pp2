6acaa794914a0c0fc85ac6dffcbcd993aced4a5a
wiltos <pawel.wiltos@gmail.com>
2023-01-21 11:05:32 +0100

    update

---
+++ ZJAZD 2 (21-22.01.2023)/INSTRUKCJA WARUNKOWA.py
@@ -0,0 +1,20 @@
+# INSTRUKCJA WARUNKOWA
+
+#  Operatory relacji -------
+# • == równości
+# • != nierówności
+# • > większości
+# • < mniejszości
+# • >= większości lub równości
+# • <= mniejszości lub równości
+
+#  Priorytety operatorów -------
+# Podstawy programowania w języku Python - Instrukcja warunkowa 3
+# Priorytet Operator
+# 1 +, - jednoargumentowe
+# 2 **
+# 3 *, /, //, %
+# 4 +, - dwuargumentowe
+# 5 <, <=, >, >=
+# 6 ==, !=
+
