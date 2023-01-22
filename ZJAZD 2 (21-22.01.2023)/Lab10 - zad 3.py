90b48b7c6eb52add1677ace2c1dce3e03401272b
wiltos <pawel.wiltos@gmail.com>
2023-01-22 15:39:49 +0100

    Update

--- ZJAZD 2 (21-22.01.2023)/Lab10 - zad 3.py
+++ ZJAZD 2 (21-22.01.2023)/Lab10 - zad 3.py
@@ -6,6 +6,8 @@
 
 # ROZWIĄZANIE PANA MARCINA:
 
+
+
 digits = [1, 2, 4, 6, 6, 2, 6, 6, 9] # zbior naszych cyfr - tu w tym zadaniu podaje z reki zbior tymczasowych elemntów
 frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]# zmienna pomocnicza, które dotyczy się listy z częstościa wystepowania poszczególnych elemntow -  z reki dajemy liste 10-elemntową
 
@@ -20,6 +22,3 @@ for i in range (len(frequency)):
         most_common = i
 
 print("Najczęściej występującą cyfrą jest " + str(most_common) + ",", "występuje", frequency[most_common], "razy.")
-
-
-# MOJE ROZWIĄZANIe
