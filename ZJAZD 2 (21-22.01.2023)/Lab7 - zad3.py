2ef4dd1aba1523ea701c186896915155f11ccbe0
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-02-02 21:46:21 +0100

    Update

--- ZJAZD 2 (21-22.01.2023)/Lab7 - zad3.py
+++ ZJAZD 2 (21-22.01.2023)/Lab7 - zad3.py
@@ -3,18 +3,17 @@
 # 3 szanse. Po nieudanej próbie, gracz powinien otrzymać podpowiedź np.
 # o parzystości zgadywanej liczby itp.
 
-# Rozwiązanie Pana MArcina -- !! sprawdzić ten kod jeszcze z filmikiem, bo coś nie do końca działa!!
 
-import random # to jest ściągnięcie biblioteki, która jest mechanizmem do loswaonia liczb (jakichs znaków)
+import random
 
-number = random.randint(1, 10)# ona nam losuje liczbę z przedziału jaki jej podamy, czyli w naszym przypadku, z przdziału od 1 do 3
-msg = "Zgadnij jaką liczbę mam na myśli (od 1 do 10) :"  # zmienna która nam poda jaka liczbę mamy na mysli
+number = random.randint(1, 10)
+msg = "Zgadnij jaką liczbę mam na myśli (od 1 do 10) :"
 guess = int(input(msg))
 if guess == number:
     print("brawo! Dokładnie taką liczbę miałem na myśli!")
 else:
-    msg = "Masz kolejną szansę :" # to jest druga szansa
-    if number % 2 == 0:# to już jest trzecia szansa, czyli podpowiedz, o parzystości
+    msg = "Masz kolejną szansę :"
+    if number % 2 == 0:
         msg += " twoja liczba jest parzysta: "
     else:
         msg += " twoja liczba jest nieparzysta: "
@@ -22,8 +21,8 @@ else:
     if guess == number:
         print("Brawo! Udało się odgadnąć za drugim razem!")
     else:
-        msg = "Masz kolejną szansę :"  # to jest druga szansa
-        if number > 5:  # to już jest trzecia szansa, czyli podpowiedz, o parzystości
+        msg = "Masz kolejną szansę :"
+        if number > 5:
             msg += "moja liczba jest większa"
         else:
             msg += "moja liczba jest mniejsza lub równa"
--- ZJAZD 2 (21-22.01.2023)/a_Przechowalnia różnych skryptów.py
+++ ZJAZD 2 (21-22.01.2023)/a_Przechowalnia różnych skryptów.py
@@ -86,4 +86,35 @@ print (str1 + ", pierwiastek kwadratowy z liczby " + str(number) + " " + str2 + 
 
 
 # ------------------------------------------------------------------------------------
-# LABORATORIUM 7 - ZAD 2
+# LABORATORIUM 7 - ZAD 3
+# Rozwiązanie Pana MArcina -- !! sprawdzić ten kod jeszcze z filmikiem, bo coś nie do końca działa!!
+
+import random # to jest ściągnięcie biblioteki, która jest mechanizmem do loswaonia liczb (jakichs znaków)
+
+number = random.randint(1, 10)# ona nam losuje liczbę z przedziału jaki jej podamy, czyli w naszym przypadku, z przdziału od 1 do 3
+msg = "Zgadnij jaką liczbę mam na myśli (od 1 do 10) :"  # zmienna, która nam poda nam podpowiedź do odgadnięcia prawdziwej liczby, widocznej pod zmienną 'number'
+guess = int(input(msg))
+if guess == number:
+    print("brawo! Dokładnie taką liczbę miałem na myśli!")
+else:
+    msg = "Masz kolejną szansę :" # to jest druga szansa
+    if number % 2 == 0:# to już jest trzecia szansa, czyli podpowiedz, o parzystości
+        msg += " twoja liczba jest parzysta: "
+    else:
+        msg += " twoja liczba jest nieparzysta: "
+    guess = int(input(msg))
+    if guess == number:
+        print("Brawo! Udało się odgadnąć za drugim razem!")
+    else:
+        msg = "Masz kolejną szansę :"  # to jest druga szansa
+        if number > 5:  # to już jest trzecia szansa, czyli podpowiedz, o parzystości
+            msg += "moja liczba jest większa"
+        else:
+            msg += "moja liczba jest mniejsza lub równa"
+        msg += " od liczby 5: "
+        guess = int(input(msg))
+    if guess == number:
+        print("Brawo! A jednak do trzech razy sztuka")
+    else:
+        msg = "Niestety myślałem o liczbie :" + str(number) + "."
+        print(msg)
