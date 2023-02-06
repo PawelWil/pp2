ace332b1eb10371c8bcc7f9a532e758be20e45b1
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-02-06 11:22:01 +0100

    Update

--- ZJAZD 2 (21-22.01.2023)/a_Przechowalnia różnych skryptów.py
+++ ZJAZD 2 (21-22.01.2023)/a_Przechowalnia różnych skryptów.py
@@ -307,3 +307,109 @@ print("{:08b}",format(number))
 print("{:08b}",format(mask))
 print("-" * 8)
 print("{:08b}",format(result))
+
+
+
+# ------------------------------------------------------------------------------------
+# LABORATORIUM 10 - ZAD 1
+
+# # ZAD. 1
+#
+# Napisz skrypt, pobierający od użytkownika zbiór imion, w tym celu:
+# • skrypt powinien zapytać użytkownika o liczbę elementów zbioru,
+# • pobrać kolejne elementy i zapisać je do listy,
+# • wyświetlić w podsumowaniu jakie imiona pobrał od użytkownika.
+
+
+# ROZWIĄZANIE PANA MARCINA:
+names  = [] # to jest zmienna na imiona
+name_number = int(input("Podaj liczbę imion: "))
+
+for i in range(1, name_number + 1): #tu to 1 przed zmienną name_number jest po to żeby liczenie nie zaczęło się od bitu liczby 0, a od 1 - gdyby było bez tego, to jakbym podał liczbe imion 3, to by mi naliczyło 4 imiona, bo zaczeło by liczyć od 0, czyli 0=imie 1, 1=imie 2, 2=imie 3, 3=imie 4
+    names.append(input("Podaj " + str(i) + " imię: "))
+
+print ("Od użytkowanika pobrano nastepujace imiona: ", names)
+
+
+
+# ------------------------------------------------------------------------------------
+# LABORATORIUM 10 - ZAD 2
+
+# # ZAD. 2
+# Napisz program, który zasymuluje 16 rzutów kostką do gry a następnie
+# wyświetli poniższe informacje:
+# • zestaw wylosowanych wyników,
+# • wyrzucaną wartość za 8 razem,
+# • liczbę wyrzuconych szóstek,
+# • maksymalną liczbę wyrzuconych tych samych wartości pod rząd.
+
+
+
+# ROZWIĄZANIE PANA MARCINA:
+
+import random
+
+dice_rolls_total = 16 #definuijemy stałą, w której będziemy przechowywać ilośc rzutów kostką - ma być 16
+rolls = [] # tu przechwujemy listę wylosowanych oczek
+
+for i in range (dice_rolls_total):
+    rolls.append(random.randint(1, 6))
+print ("Zbior wyników po", dice_rolls_total, "rzutach kostką do gry:", rolls) # to jest podpunkt 1
+
+print ("Za 8 razem wyrzucono wartość", rolls[8 - 1] ) # to jest podpunkt 2 bez kropki po wartosc
+print ("Za 8 razem wyrzucono wartość", str(rolls[8 - 1]) + "." ) # to jest podpunkt 2, ale z kropką po 'wartość'
+
+# podpunkt 3
+sixes_total = 0 # na początku tą zmienną pomocniczą ustawiamy na 0
+for roll in rolls:
+    if roll == 6:
+        sixes_total += 1 # to jestzwiakszenie za pomoca opreattoar '+=' o 1
+print("Wyrzucono", sixes_total, "razy szostke")
+
+# podpunkt 4
+in_row_value_tmp = rolls[0] #zmienna pod rząd, ale pomocnicza = tmp=temporary
+in_row_total_tmp = 0 # pod rząd total, tez pomocnicza = tmp=temporary
+
+in_row_value = 0 #tu juz nie pomocnicze, tylko koncowe
+in_row_total = 0 # tu juz nie pomocnicze, tylko koncowe
+
+for roll in rolls:
+    if(roll == in_row_value_tmp):
+        in_row_total_tmp += 1 # in_row_total_tmp zwiaksza sie za pomoca opreattoar '+=', o 1
+    else:
+        in_row_value_tmp = roll
+        in_row_total_tmp = 1
+    if in_row_total_tmp > in_row_total:
+        in_row_total = in_row_total_tmp
+        in_row_value = in_row_value_tmp
+
+print ("Pod rząd wyrzucono: ", in_row_total, "razy wartość", in_row_value)
+print ("Pod rząd wyrzucono: ", in_row_total, "razy wartość", str(in_row_value) + ".") # to jest to samo co u góry, ale z kropką po "wartość"
+
+
+
+# ------------------------------------------------------------------------------------
+# LABORATORIUM 10 - ZAD 3
+
+# # ZAD. 3
+
+# Napisz skrypt, który znajdzie najczęściej występującą cyfrę w zbiorze.
+# Np. w zbiorze [4, 1, 2, 9, 4, 4] najczęściej występującą cyfrą jest 4,
+# występuje 3 razy.
+
+# ROZWIĄZANIE PANA MARCINA:
+
+
+
+digits = [1, 2, 4, 6, 6, 2, 6, 6, 9] # zbior naszych cyfr - tu w tym zadaniu podaje z reki zbior tymczasowych elementów
+frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]# zmienna pomocnicza, które dotyczy się listy z częstościa wystepowania poszczególnych elemntow -  z reki dajemy liste 10-elemntową
+
+for digit in digits:
+    frequency[digit] += 1
+
+print(frequency)
+
+most_common = -1
+for i in range (len(frequency)):
+    if frequency[i] > most_common:
+        most_common = i
