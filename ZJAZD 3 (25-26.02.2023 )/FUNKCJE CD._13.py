8df1fecb16536ea60dba8e88861d466a39a793d9
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-03-22 19:23:54 +0100

    Update

--- ZJAZD 3 (25-26.02.2023 )/FUNKCJE CD._13.py
+++ ZJAZD 3 (25-26.02.2023 )/FUNKCJE CD._13.py
@@ -2,7 +2,7 @@
 
 
 # 1. jak wspoładziaja listy razem z funkcjami-----------
-# definiujemy funkcje, która będzie zliczać sumę elementów naszej listy przekazanej do finkcji
+# definiujemy funkcje, która będzie zliczać sumę elementów naszej listy przekazanej do funkcji
 # def sum_numbers(numbers): # to co jest w nawiasie to są parametry tej fukcji, tutaj zmienną 'numbers'
 #     sum = 0 # tu dajemy zmienną lokalną 'sum'
 #     for element in numbers: #teraz korzystamy z petli, ktora to zmienna dostanie kolejne elementy, naszego obiektu, kótrym jest lista
@@ -13,7 +13,7 @@
 # -- 1 sposób na pokazanie sumy - bezpośredni sposób
 # print (sum_numbers([1,2,3]))
 
-# -- 2 sposób na pokazanie sumy - dekomponuemy na mniejsze elemnty: dajemy zmienną 'numbers', gdzie przypiszemy listę
+# -- 2 sposób na pokazanie sumy - dekomponuemy na mniejsze elementy: dajemy zmienną 'numbers', gdzie przypiszemy listę
 #następnie zmienna 'result' gdzie do funkcji sum_numbers wrzucamy listę + finalnie drukujemy za pomocą print tą zmienną 'result'
 #w której zawarta jest lista.
 # numbers = [1,2,3]# to jest zmienna globalna 'numbers'
@@ -36,7 +36,8 @@
 #         będziemy losowac liczby w zakresie miedzy 1 a 99.
 #     return numbers # A teraz po losowaniu zrwacamy naszą listę o nazwie 'numbers'
 
-# print (generate_numbers(10)) # używamy naszej funkcji 'generate_numbers' za pomocą ktorej (oczywiście zeby się te liczby wylosowane wyświetliły, musimy uzyć funkcji drukującej 'print')
+# print (generate_numbers(10)) # używamy naszej funkcji 'generate_numbers' za pomocą ktorej
+# (oczywiście zeby się te liczby wylosowane wyświetliły, musimy uzyć funkcji drukującej 'print')
 # w tym przypadku wylosuje nam 10 liczb, z zakresu od 1 do 99 i pokaze na ekranie
 # print (generate_numbers(100)) # tu nam wylosuje 100 liczb
 # print (generate_numbers(123233)) # a tu nam wylosuje 123233 liczb
@@ -89,31 +90,63 @@
 # show_me_recusrion(20)
 
 
-# przyklad 1
+# przyklad 1 - tylko zmienna lokalna x, której wartość bez funkcji print się nie wyświetli
 # def scope_test (): # ma jedną zmienną lokalką x - to jest x poniżej
 #     x = 123
 # scope_test() # to jest uruchomienie tej funkcji - dzuiala ale sie nic nie pojawia , bo retiurn zwraca wartosc, a nie pokazjuje jak print
 # print(x) # tak nam nie wyswietli bo print jest uznawany jako globalny i tu bedzie error
 
-# przyklad 2---------
+# przykład 1' - też tylko zmienna lokalna x, ale już z funkcją print w ciele funkcji i dzięki temu jej wartość się wyświetli
 # def scope_test (): # ma jedną zmienną lokalką x - to jest x poniżej
-#     print(" wsrodku funkji:" + str(x))
+#     x = 123
+#     print(x)
+# scope_test() # to jest uruchomienie tej funkcji - dzuiala ale sie nic nie pojawia , bo retiurn zwraca wartosc, a nie pokazjuje jak print
+
+# przykład 1'' - tu zmienna lokalna x + globalna y, i dzięki funkcji print w ciele funkcji wartość zmiennej lokalnej się wyświetli,
+# + dzięki globalnej funkcji print, wartość zmiennej y też się wyświetli
+# def scope_test (): # ma jedną zmienną lokalką x - to jest x poniżej
+#     x = 123
+#     print(x)
+# y=9
+# scope_test() # to jest uruchomienie tej funkcji - dzuiala ale sie nic nie pojawia , bo retiurn zwraca wartosc, a nie pokazjuje jak print
+# print(y)
+
+
+
+
+# przyklad 2--------- Wyświetlenie zmiennej globalnej w ciele funkcji (w przypadku, gdy nie ma zmiennej lokalnej, a nazwa zmiennej
+# globalnej została użyta w ciele funkcji)
+# def scope_test (): # ma jedną zmienną globalną x - to jest x poniżej, która jesli nie ma zmiennej lokalne a ma taką samą nazwę
+    # zmiennej jak w ciele funkcji, to ją wyświetli
+    # print(" wsrodku funkji:" + str(x))
 #
 # x = 99 # zmienna globalna 99
 # scope_test() # jak to uruchmimy tą funkcje poprzez scope_test i mamy u gory print, to jak widać te 99 się pokazało
 
-# # przyklad 3------- ZASIĘG ZMIEnnych CD.
-def scope_test (): # definujemy funkcje o nazwie scope_test ,  ma on ajedną zmienną lokalką x - to jest x poniżej,
+# przyklad 2'--------- Pierwszenstwo dla wyświetlenie zmiennej lokalnej, zamiast globalnej, gdzie zmienne mają tą samą nazwę,
+# ale funkcja print jest w ciele funckji
+# def scope_test ():
+#     x = 8 # to jest zmienna lokalna,która się wyświetli
+#     print(" wsrodku funkji:" + str(x))
+
+# x = 99 # zmienna globalna 99
+# scope_test() # jak to uruchmimy tą funkcje poprzez scope_test i mamy u gory print, to jak widać te 99 się pokazało
+
+
+
+# # przyklad 3------- ZASIĘG ZMIEnnych CD. - przewaga zmiennej loklanej nad globalną, dla fukcji print w ciele funkcji
+# oraz wyswietlenie zmiennej globalnej za pomocą globalnej funkcji print
+# def scope_test (): # definujemy funkcje o nazwie scope_test ,  ma ona jedną zmienną lokalną x - to jest x poniżej,
 # do której przypiszemy wartość 123
-    x=123 # zmienna lokalna
-    print(" wsrodku funkji:" + str(x)) # to wyswietla niezaleznie dla globalnej lokalnej x = 123
+#     x=123 # zmienna lokalna
+#     print(" wsrodku funkji:" + str(x)) # to wyswietla niezaleznie dla globalnej lokalnej x = 123
 
 # x = 99 # zmienna globalna 99
 # scope_test() # jak to uruchmimy tą funkcje poprzez scope_test i mamy u gory print, to jak widać te 99 się pokazało
 # print(" na zewnatrz: " + str(x)) # to wyswietla niezaleznie dla globalnej zmiennenn x = 99
 
 
-# przyklad 4 - tu wplywamy na zmienną globalną i znienić ją w ciele funkcji (nadpisujemy zmienną globalną,
+# przyklad 4 - tu wplywamy na zmienną globalną i zmieniamy ją w ciele funkcji (nadpisujemy zmienną globalną,
 # za pomocą zmiennej w lokalnej w ciele funkcji---------
 # def scope_test ():
 #     global x # zeby moc zamienic zmienną globalną trzeba uzyc instrukcji global, która zacznie traktować zmienną x,
@@ -160,12 +193,13 @@ def scope_test (): # definujemy funkcje o nazwie scope_test ,  ma on ajedną zmi
 ## Kolejny przyklad - zmien wartosc listy, która jest listą mającą element 0 [0] i podstaw za nią wartos 999--------------
 # def change_value(n):
 #     print ("przed zmiana:", n)
-#     n[0] = 999 # tu bedziemy miec: [999, 2] pod n, w indeksie 0 podstawilismy całkeim inny obiekt, ale lokalnie,
+    # n[0] = 999 # tu bedziemy miec: [999, 2] pod n, w indeksie 0 podstawilismy całkeim inny obiekt, ale lokalnie,
     # który ma pierwszenstow nad globalną, dlatego [1,2]=przed zmianą, zostało zastąpione [999,2]=po zmianie
     # print("po zmianie", n)
 
 # val = [1,2] # do tej zmiennej globalnej przeakuzjemy liste
-# change_value(val) # za pomocą tej change_value mozemy zmienić wartość listy 'val=[1,2] - i następnie program przekazuje do piewrszej linii , tak gdzie funkcja def jest opisana , nawet jak jest n ale tu już jest val i to wchodzi zamiast n
+# change_value(val) # za pomocą tej change_value mozemy zmienić wartość listy 'val=[1,2] - i następnie program przekazuje do piewrszej linii ,
+# tam gdzie funkcja change_value jest opisana , nawet jak jest n ale tu już jest val i to wchodzi zamiast n
 # print(val)
 
 
@@ -181,9 +215,9 @@ def scope_test (): # definujemy funkcje o nazwie scope_test ,  ma on ajedną zmi
 # będzie w sobie zawierał, czyli będzie zawierał liczby od 1 do 8
 
 ### 2. to jest przekazywanie wielu argumentow, jako argumentów przekazywanych przez nazwe, a nie przez pozycję--------
-# ????
+
 # def my_func(**args):
 #     for el in args.items():
 #         print(el)
-#
+
 # my_func(val1 = "raz", val2 = 999) # teraz tą funkcje 'my func' sobie wywolujemy!!;
